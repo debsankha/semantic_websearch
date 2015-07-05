72162211097c53edaffed38f01d6ac85fcfbd486
debsankha manik <dmanik@nld.ds.mpg.de>
2015-07-05 22:51:59 +0200

    added output of sample.py

--- sample.py
+++ sample.py
@@ -11,7 +11,7 @@ os.environ['STANFORD_MODELS'] = jarpath
 
 parser = stanford.StanfordParser(model_path=english_modelpath)
 
-parsed=parser.raw_parse_sent("The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices or walk home during the night, officials said today.")
+parsed=parser.raw_parse("The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices or walk home during the night, officials said today.")
 
 for node in parsed:
     print node
